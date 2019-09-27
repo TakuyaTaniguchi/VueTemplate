83ca789cefba0c8e25e19cf337f9e8cb106535f0
TakuyaTaniguchi <ttaniguchi@oikaze.jp>
2019-09-28 00:25:57 +0900

    Update Pageテンプレート

---
+++ src/components/Page.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Hello page</h1>
+</template>
+
+<script>
+export default {
+  name: 'Page',
+  data () {
+    return {
+      msg: 'Welcome to Page'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
